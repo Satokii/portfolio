9239455943aac67ae34c609af8071efc28046f23
Satoki <satokiito@gmail.com>
2023-11-27 11:21:28 +0000

    removed redundant css and re-styled links in main projects section

--- src/components/main/components/MainProjects.jsx
+++ src/components/main/components/MainProjects.jsx
@@ -23,7 +23,7 @@ function MainProjects() {
                         <div className='project-info--description grid'>
                             <h4>Description</h4>
                             <p>I built this social media style community post dashboard from scratch as a part of my Boolean bootcamp. I used a reference image provided by Boolean to guide the styling of the application. <br /> <br /> A user can create a new post, delete existing comments and posts, create a new user and also update existing user details. <br /> <br />I used an API as my data source and used the relevant API methods in order to send relevant requests to achieve the aforementioned behaviours.</p>
-                            <p className='project-info--description--link'>Click here to view more about this app!</p>
+                            <p className='project-info--description--link'>Further project details</p>
                         </div>
                         <div className='project-info--tech grid'>
                             <h4 className='project-info--tech-header'>Tech Used</h4>
@@ -41,7 +41,7 @@ function MainProjects() {
                         <img className='project-image--image' src={DashboardMain} alt="dashboard project main photo"/>
                         <div className='project-image--github-link-container grid'>
                             <a className='project-image--github-link grid' target='_blank' href="https://github.com/Satokii/react-cohort-dashboard-challenge" rel="noreferrer">
-                                Github Link
+                                Github
                             </a>
                             <img className='github-link-logo' src={GithubLogo} alt="github logo" width={25} />
                         </div>
@@ -55,7 +55,7 @@ function MainProjects() {
                         <img className='project-image--image' src={Placeholder} alt="placeholder" width={600}/>
                         <div className='project-image--github-link-container grid'>
                             <a className='project-image--github-link grid' target='_blank' href="#" rel="noreferrer">
-                                Github Link
+                                Github
                             </a>
                             <img className='github-link-logo' src={GithubLogo} alt="github logo" width={25} />
                         </div>
@@ -75,7 +75,7 @@ function MainProjects() {
                         <div className='project-info--description grid'>
                             <h4>Description</h4>
                             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla quis eum ratione deserunt, distinctio blanditiis sint vero veniam minima cum perspiciatis maiores est provident quae, eius sed architecto id enim. <br /> <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis error voluptatem porro officiis doloribus non, corrupti qui corporis assumenda explicabo itaque excepturi veniam illo voluptatibus, facilis quam! Qui, harum corporis. <br /> <br />Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque explicabo repellat doloribus consectetur tempore, delectus pariatur accusamus, eaque necessitatibus cum ipsum id iure libero repellendus corporis eligendi quia provident molestiae!</p>
-                            <p className='project-info--description--link'>Click here to view more about this app!</p>
+                            <p className='project-info--description--link'>Further project details</p>
                         </div>
                     </section>
                 </article>
@@ -86,7 +86,7 @@ function MainProjects() {
                         <div className='project-info--description grid'>
                             <h4>Description</h4>
                             <p>🛠️ Work is still under construction. Please check back again later 🛠️</p>
-                            <p className='project-info--description--link'>Click here to view more about this app!</p>
+                            <p className='project-info--description--link'>Further project details</p>
                         </div>
                         <div className='project-info--tech grid'>
                             <h4 className='project-info--tech-header'>Tech Used</h4>
@@ -102,7 +102,7 @@ function MainProjects() {
                         <img className='project-image--image' src={WorkInProgress} alt="under construction" width={500}/>
                         <div className='project-image--github-link-container grid'>
                             <a className='project-image--github-link grid' target='_blank' href="#" rel="noreferrer">
-                                Github Link
+                                Github
                             </a>
                             <img className='github-link-logo' src={GithubLogo} alt="github logo" width={25} />
                         </div>
