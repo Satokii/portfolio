474090921d2f85749855c7fb8a7cdd8c91adb340
Satoki <satokiito@gmail.com>
2024-03-09 23:10:10 +0000

    refactor: remove links to nav header li's, links now jump to page section

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,6 @@
-import { useNavigate } from 'react-router-dom'
 import '../../styles/header/header.css'
 
 function Header() {
-
-    const navigate = useNavigate()
 
     function jumpToTop() {
         window.scrollTo(0, 0)
@@ -12,23 +9,16 @@ function Header() {
     return (
         <header className='header grid'>
             <h1 className='logo' onClick={() => {
-                navigate('/')
                 jumpToTop()
             }}>Satoki Ito</h1>
             <nav className='header-nav grid'>
                 <ul className='header-nav-list grid'>
-                    <li className='header-home' onClick={() => {
-                        navigate('/')
-                        jumpToTop()
-                    }}>Home</li>
-                    <li className='header-about-me' onClick={() => {
-                        navigate('/about-me')
-                        jumpToTop()
-                    }}>About Me</li>
-                    <li className='header-projects' onClick={() => {
-                        navigate('/projects')
-                        jumpToTop()
-                    }}>Projects</li>
+                    <li className='header-about-me'>
+                        <a href="#about-me">About Me</a>
+                    </li>
+                    <li className='header-projects'>
+                        <a href="#projects">Projects</a>
+                    </li>
                 </ul>
             </nav>
         </header>
