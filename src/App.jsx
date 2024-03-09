c565fab756930b1700b03ff03409209833b4f682
Satoki <satokiito@gmail.com>
2024-03-09 23:12:09 +0000

    refactor: removed about me and projects component pages and associated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
 import Header from './components/header'
 import Main from './components/main'
-import AboutMe from './components/about-me'
-import Projects from './components/projects'
 import Footer from './components/footer'
 import './App.css'
 
@@ -11,23 +8,7 @@ function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route
-          path='/'
-          element={<Main />}
-        >
-        </Route>
-        <Route
-          path='/about-me'
-          element={<AboutMe />}
-        >
-        </Route>
-        <Route
-          path='/projects'
-          element={<Projects />}
-        >
-        </Route>
-      </Routes>
+      <Main></Main>
       <Footer />
     </>
   )
