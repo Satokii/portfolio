6a2ae8fa828ea1647ee2ab0c6a2eb6deec4eab0a
Satoki <satokiito@gmail.com>
2024-03-09 23:55:13 +0000

    refactor: shuffled order of display of details on overview page

--- src/components/main/components/Overview.jsx
+++ src/components/main/components/Overview.jsx
@@ -11,20 +11,20 @@ function Overview() {
                         <h2 className='overview-header grid'>Satoki Ito</h2>
                         <h3 className='overview-job-header'>Front End Developer</h3>
                     </header>
+                    <div className='profile-photo-container grid'>
+                        <img className='profile-photo grid' src={ProfilePhoto} alt="profile photo" width={380} />
+                    </div>
+                </div>
+                <div className='overview-right grid'>
                     <div className='overview-quick-links grid'>
                         <a className='grid' href="#about-me">About Me</a>
                         <a className='grid' href='#projects'>My Projects</a>
                     </div>
-                </div>
-                <div className='overview-right grid'>
-                    <div className='profile-photo-container grid'>
-                        <img className='profile-photo grid' src={ProfilePhoto} alt="profile photo" width={380} />
-                    </div>
-                    <div className='overview-text-container grid'>
+                    {/* <div className='overview-text-container grid'>
                         <p className='overview-text'>
                             Hi, my name is Satoki. <br /> I am a student on the Boolean Software Development bootcamp, currently working towards becoming a Full-Stack Developer.
                         </p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </section>
