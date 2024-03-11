715961ac2abd539434e4c8579474fa6e065122cd
Satoki <satokiito@gmail.com>
2024-03-11 14:21:51 +0000

    feat: add email contact details

--- src/components/main/components/Contact.jsx
+++ src/components/main/components/Contact.jsx
@@ -1,10 +1,15 @@
+import EmailImg from '../../../assets/svgs/email.svg'
+import '../../../styles/main/main-contact.css'
+
 function Contact() {
 
     return (
         <section id='contact' className='main-contact grid'>
-            <h2 className='main-projects--header'>Contact</h2>
-            <div>
-                <p>Email: satokiito@gmail.com</p>
+            <h2 className='main-contact--header'>Contact</h2>
+            <h3 className='main-contact--subheader'>Get in touch. Let&apos;s connect!</h3>
+            <div className='main-contact--contact-info-container grid'>
+                <img className='main-contact--email-img' src={EmailImg} alt="email img" />
+                <p className='main-contact--email'>satokiito@gmail.com</p>
             </div>
         </section>
     )
