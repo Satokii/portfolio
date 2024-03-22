ff6b4f9d06d3f53dd5a7fec5993d60f50bbff113
Satoki <satokiito@gmail.com>
2024-03-22 15:09:09 +0000

    style: add basic styling to contact form

--- src/components/main/components/Contact.jsx
+++ src/components/main/components/Contact.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import emailjs from '@emailjs/browser'
 
-import EmailImg from '../../../assets/svgs/email.svg'
 import '../../../styles/main/main-contact.css'
 
 const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
@@ -42,34 +41,33 @@ function Contact() {
         <section id='contact' className='main-contact grid'>
             <h2 className='main-contact--header'>Contact</h2>
             <h3 className='main-contact--subheader'>Get in touch. Let&apos;s connect!</h3>
-            {/* <div className='main-contact--contact-info-container grid'>
-                <img className='main-contact--email-img' src={EmailImg} alt="email img" />
-                <p className='main-contact--email'>satokiito@gmail.com</p>
-            </div> */}
-            <form onSubmit={onSubmit}>
-                <input
-                    type='text'
-                    name='from_name'
-                    placeholder='Your name'
-                    value={toSend.from_name}
-                    onChange={handleChange}
-                />
-                <input
-                    type='text'
-                    name='message'
-                    placeholder='Your message'
-                    value={toSend.message}
-                    onChange={handleChange}
-                />
-                <input
-                    type='text'
-                    name='from_email'
-                    placeholder='Your email'
-                    value={toSend.from_email}
-                    onChange={handleChange}
-                />
-                <button type='submit'>Submit</button>
-            </form>
+            <div className='main-contact--contact-info-container grid'>
+                <form className='main-contact--contact-form' onSubmit={onSubmit}>
+                    <input
+                        type='text'
+                        name='from_name'
+                        placeholder='Your name'
+                        value={toSend.from_name}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type='email'
+                        name='from_email'
+                        placeholder='Your email'
+                        value={toSend.from_email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type='text'
+                        name='message'
+                        placeholder='Your message'
+                        value={toSend.message}
+                        onChange={handleChange}
+                    />
+                    
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
         </section>
     )
 }
