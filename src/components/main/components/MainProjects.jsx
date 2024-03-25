949d56e925e6e35e15104ec632d7d5f717946648
Satoki <satokiito@gmail.com>
2024-03-25 15:06:12 +0000

    refactor: change video source format to mp4

--- src/components/main/components/MainProjects.jsx
+++ src/components/main/components/MainProjects.jsx
@@ -36,11 +36,11 @@ function MainProjects() {
                     <div className='project-video--container grid'>
                         <div>
                             <h4 className='project-sub-header'>Music Playback and Volume Controls Demo:</h4>
-                            <video preload='metadata' controls src={SpotifyPlaybackVid} type="video/webm" ></video>
+                            <video preload='auto' controls src={SpotifyPlaybackVid} type="video/mp4" ></video>
                         </div>
                         <div>
                             <h4 className='project-sub-header'>Further Details and Search Feature Demo:</h4>
-                            <video preload='metadata' controls src={SpotifyDetailsVid} type="video/webm" ></video>
+                            <video preload='auto' controls src={SpotifyDetailsVid} type="video/mp4" ></video>
                         </div>
                     </div>
                     <a className='project-links-container grid' target='_blank' href="#" rel="noreferrer">
@@ -72,11 +72,11 @@ function MainProjects() {
                     <div className='project-video--container grid'>
                         <div>
                             <h4 className='project-sub-header'>Create New Workout Demo:</h4>
-                            <video preload='metadata' controls src={WorkoutAddWorkoutVid} type="video/webm" ></video>
+                            <video preload='auto' controls src={WorkoutAddWorkoutVid} type="video/mp4" ></video>
                         </div>
                         <div>
                             <h4 className='project-sub-header'>App Navigation Demo:</h4>
-                            <video preload='metadata' controls src={WorkoutNavVid} type="video/webm" ></video>
+                            <video preload='auto' controls src={WorkoutNavVid} type="video/mp4" ></video>
                         </div>
                     </div>
                     <a className='project-links-container grid' target='_blank' href="https://github.com/Satokii/workout-app" rel="noreferrer">
