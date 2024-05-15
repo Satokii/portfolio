9fad081f34cd01c628ce57058bd5c56444d93640
Satoki <satokiito@gmail.com>
2024-05-15 23:20:25 +0100

    feat: add _blank target attribute to contact section linkedin link

--- src/components/main/components/Contact.jsx
+++ src/components/main/components/Contact.jsx
@@ -70,7 +70,7 @@ function Contact() {
                     <button type='submit'>Submit</button>
                 </form>
                 <div className='main-contact--linkedin-container grid'>
-                    <a className='main-contact--linkedin-link grid' href="https://www.linkedin.com/in/satoki-ito-470133241/">
+                    <a className='main-contact--linkedin-link grid' target='_blank'  href="https://www.linkedin.com/in/satoki-ito-470133241/" rel="noreferrer">
                         <p className='main-contact--linkedin-text'> Connect with me</p>
                         <img className='main-contact--linkedin-logo' src={LinkedInLogo} alt="linkedin logo" />
                     </a>
