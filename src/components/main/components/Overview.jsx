9befb6072b30c1f2d4ff8cde627d189de781ed95
Satoki <satokiito@gmail.com>
2024-07-04 15:00:08 +0100

    refactor: remove overview subheader

--- src/components/main/components/Overview.jsx
+++ src/components/main/components/Overview.jsx
@@ -9,7 +9,6 @@ function Overview() {
                 <div className='overview-left grid'>
                     <header className='overview-header-container grid'>
                         <h2 className='overview-header grid'>Satoki Ito</h2>
-                        <p className='overview-sub-detail-header'>Former banker, now</p>
                         <h3 className='overview-job-header'>Full-Stack Developer</h3>
                     </header>
                     <div className='profile-photo-container grid'>
