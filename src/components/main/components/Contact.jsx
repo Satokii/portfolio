f85cea44652d0cced3e2804511d56c0021710784
Satoki <satokiito@gmail.com>
2024-07-07 19:26:06 +0100

    refactor: remove redundant code following refactor

--- src/components/main/components/Contact.jsx
+++ src/components/main/components/Contact.jsx
@@ -1,42 +1,8 @@
-import { useState } from 'react'
-import emailjs from '@emailjs/browser'
 import LinkedInLogo from '../../../assets/misc/linkedin-logo.webp'
 import '../../../styles/main/main-contact.css'
 import ContactForm from './ContactForm';
 
-const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
-const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
-const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
-
-
 function Contact() {
-    // const [toSend, setToSend] = useState({
-    //     from_name: '',
-    //     message: '',
-    //     from_email: '',
-    // });
-    
-    // const onSubmit = (e) => {
-    //     e.preventDefault();
-       
-    //     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
-    //     .then(() => {
-    //         alert('Message Sent Successfully')
-    //     }, (error) => {
-    //         console.log(error.text);
-    //         alert('Something went wrong!')
-    //     });
-       
-    //     setToSend({
-    //         from_name: '',
-    //         message: '',
-    //         from_email: '',
-    //     })
-    //     };
-    
-    // const handleChange = (e) => {
-    //     setToSend({ ...toSend, [e.target.name]: e.target.value });
-    // };
 
     return (
         <section id='contact' className='main-contact grid'>
@@ -44,33 +10,6 @@ function Contact() {
             <h3 className='main-contact--subheader'>Get in touch!</h3>
             <div className='main-contact--contact-info-container grid'>
                 <ContactForm />
-                {/* <form className='main-contact--contact-form grid' onSubmit={onSubmit}>
-                    <input
-                        type='text'
-                        name='from_name'
-                        placeholder='Your name'
-                        value={toSend.from_name}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type='email'
-                        name='from_email'
-                        placeholder='Your email'
-                        value={toSend.from_email}
-                        onChange={handleChange}
-                    />
-                    <textarea 
-                        name="message" 
-                        id="message" 
-                        placeholder='Your message'
-                        cols="30" 
-                        rows="5"
-                        value={toSend.message}
-                        onChange={handleChange}
-                    >
-                    </textarea>                    
-                    <button type='submit'>Submit</button>
-                </form> */}
                 <div className='main-contact--linkedin-container grid'>
                     <a className='main-contact--linkedin-link grid' target='_blank'  href="https://www.linkedin.com/in/satoki-ito-470133241/" rel="noreferrer">
                         <p className='main-contact--linkedin-text'> Connect with me</p>
