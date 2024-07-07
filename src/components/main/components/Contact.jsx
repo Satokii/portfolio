c35e3fed848165194946fd60d63346c47c0022a2
Satoki <satokiito@gmail.com>
2024-07-07 19:26:36 +0100

    style: formatting

--- src/components/main/components/Contact.jsx
+++ src/components/main/components/Contact.jsx
@@ -1,24 +1,32 @@
-import LinkedInLogo from '../../../assets/misc/linkedin-logo.webp'
-import '../../../styles/main/main-contact.css'
-import ContactForm from './ContactForm';
+import LinkedInLogo from "../../../assets/misc/linkedin-logo.webp";
+import ContactForm from "./ContactForm";
+import "../../../styles/main/main-contact.css";
 
 function Contact() {
-
-    return (
-        <section id='contact' className='main-contact grid'>
-            <h2 className='main-contact--header'>Contact</h2>
-            <h3 className='main-contact--subheader'>Get in touch!</h3>
-            <div className='main-contact--contact-info-container grid'>
-                <ContactForm />
-                <div className='main-contact--linkedin-container grid'>
-                    <a className='main-contact--linkedin-link grid' target='_blank'  href="https://www.linkedin.com/in/satoki-ito-470133241/" rel="noreferrer">
-                        <p className='main-contact--linkedin-text'> Connect with me</p>
-                        <img className='main-contact--linkedin-logo' src={LinkedInLogo} alt="linkedin logo" />
-                    </a>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section id="contact" className="main-contact grid">
+      <h2 className="main-contact--header">Contact</h2>
+      <h3 className="main-contact--subheader">Get in touch!</h3>
+      <div className="main-contact--contact-info-container grid">
+        <ContactForm />
+        <div className="main-contact--linkedin-container grid">
+          <a
+            className="main-contact--linkedin-link grid"
+            target="_blank"
+            href="https://www.linkedin.com/in/satoki-ito-470133241/"
+            rel="noreferrer"
+          >
+            <p className="main-contact--linkedin-text"> Connect with me</p>
+            <img
+              className="main-contact--linkedin-logo"
+              src={LinkedInLogo}
+              alt="linkedin logo"
+            />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/main/components/ContactForm.jsx
+++ src/components/main/components/ContactForm.jsx
@@ -1,38 +1,40 @@
-import { useState } from 'react'
-import emailjs from '@emailjs/browser'
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
 
 function ContactForm() {
-    const [toSend, setToSend] = useState({
-        from_name: '',
-        message: '',
-        from_email: '',
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    message: "",
+    from_email: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then(
+      () => {
+        alert("Message Sent Successfully");
+      },
+      (error) => {
+        console.log(error.text);
+        alert("Something went wrong!");
+      }
+    );
+
+    setToSend({
+      from_name: "",
+      message: "",
+      from_email: "",
     });
-    
-    const onSubmit = (e) => {
-        e.preventDefault();
-       
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
-        .then(() => {
-            alert('Message Sent Successfully')
-        }, (error) => {
-            console.log(error.text);
-            alert('Something went wrong!')
-        });
-       
-        setToSend({
-            from_name: '',
-            message: '',
-            from_email: '',
-        })
-        };
-    
-    const handleChange = (e) => {
-        setToSend({ ...toSend, [e.target.name]: e.target.value });
-    };
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
 
   return (
     <form className="main-contact--contact-form grid" onSubmit={onSubmit}>
